41fc69b0b50d8facd7547a872fc580391de33cdf
Max Greenwald <maxgreenwald@me.com>
2017-03-19 20:38:24 -0400

    added infastructure for Flask-SQLAlchemy using PostgreSQL

---
+++ config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/doctor_reviews"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
